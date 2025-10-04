f4beb8394af02147dfb753c66f1f2b77e50e242b
devopspty19 <50472768+devopspty19@users.noreply.github.com>
2025-10-03 23:41:18 -0500

    Create HelloMIDlet.java

---
+++ HelloMIDlet.java
@@ -0,0 +1,13 @@
+//java
+// Ejemplo J2ME MIDlet básico
+public class HelloMIDlet extends MIDlet {
+    public void startApp() {
+        Display display = Display.getDisplay(this);
+        Form form = new Form("Hello World");
+        form.append("¡Hola desde J2ME!");
+        display.setCurrent(form);
+    }
+    
+    public void pauseApp() {}
+    public void destroyApp(boolean unconditional) {}
+}
