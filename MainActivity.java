a91f6107ee48242f46952097ecd67a6903bb983b
devopspty19 <50472768+devopspty19@users.noreply.github.com>
2025-10-04 00:00:56 -0500

    Create MainActivity.java

---
+++ MainActivity.java
@@ -0,0 +1,11 @@
+/* Java clásico para Android */
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        TextView textView = findViewById(R.id.textView);
+        textView.setText("Hello Android");
+    }
+}
